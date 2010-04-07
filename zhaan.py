3a5d8dd9027512fa0a92e39a182d4a1487507d5a
Zach Goldberg <zgold@corvette.(none)>
2010-04-07 02:13:14 -0400

    This code was moved into its own file a long time ago.  It no longer needs to be in Zhaan.py

--- zhaan.py
+++ zhaan.py
@@ -9,26 +9,6 @@ from action import UPnPAction
 from DIDLParser import DIDLParser
 from UPnPDeviceManager import UPnPDeviceManager
 
-class DIDLParser(object):
-  def __init__(self,  xml_data):
-    self.containers = []
-    self.objects = []
-
-    parser = GUPnPAV.GUPnPDIDLLiteParser()
-    parser.connect("container_available", self.new_container)
-    parser.connect("item_available", self.new_item)
-    try:
-      parser.parse_didl(xml_data)
-    except:
-      pass
-        
-  def new_item(self, node, object):
-    self.objects.append(object)
-
-  def new_container(self, node, object):
-    self.containers.append(object)
-
-
 class PyGUPnPCP(object):
   def __init__(self):
     self.devices = []
