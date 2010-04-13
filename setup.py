823460c389afee71083b8e7b2f8b301e8e6fc800
Zach Goldberg <zgold@bluesata.(none)>
2010-04-13 01:32:02 -0400

    Add build system & packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Zhaan',
+      version='0.1',
+      description='Zhaan UPnP Control Point',
+      author='Zach Goldberg',
+      author_email='zach@zachgoldberg.com',
+      url='http://www.zachgoldberg.com/projects/zhaan/',
+      packages=['zhaan', 'zhaan.gui'],
+     )
